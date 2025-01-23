2d7d27b2c72ef07d2698168218350a0b66ed9907
Aleksa Perić <alex.peric1205@gmail.com>
2025-01-23 11:08:27 +0100

    Update NavLinks.tsx

--- Projekat I - Neo4J/Frontend/src/components/NavLinks.tsx
+++ Projekat I - Neo4J/Frontend/src/components/NavLinks.tsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import UseAuth from "../hooks/useAuth";
+import { Roles } from "../api/Roles";
 
 const navigation = [
   { name: "Homepage", link: "/" },
@@ -7,9 +9,25 @@ const navigation = [
 ];
 
 const NavLinks = () => {
+  const { auth } = UseAuth();
   return (
     <div className="hidden w-full md:block md:w-auto " id="navbar-default">
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+        {auth.role == Roles.Admin && (
+          <li>
+            <NavLink
+              to="/admin-dashboard"
+              className={({ isActive }) => {
+                return (
+                  "rounded-md block py-2 px-5 " +
+                  (isActive ? "my-active" : "my-text-gray")
+                );
+              }}
+            >
+              Admin Dashboard
+            </NavLink>
+          </li>
+        )}
         {navigation.map((item) => (
           <li key={item.name}>
             <NavLink
